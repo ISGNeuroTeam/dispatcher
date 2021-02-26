235abdf5f96eec174f71ca7dab8520ab7805b205
astarchenkov <astarchenkov@isgneuro.com>
2021-02-26 19:12:42 +0300

    Added mode "as-is" to OTLCollect.scala.

--- src/main/scala/ot/scalaotl/commands/OTLCollect.scala
+++ src/main/scala/ot/scalaotl/commands/OTLCollect.scala
@@ -36,32 +36,31 @@ class OTLCollect(sq: SimpleQuery) extends OTLBaseCommand(sq) with OTLIndexes {
 
     keywordsMap.get("index") match {
       case Some(Keyword(_,i)) =>
-       // var res = keywordsMap.foldLeft(_df){case (df, (_,Keyword(k,v))) => df.withColumn(k,lit(v))}
-        var res = keywordsMap.get("host") match {
-          case Some(Keyword(_,v)) => _df.withColumn("_host",lit(v))
-          case _=>_df
-        }
-        res = keywordsMap.get("source") match {
-          case Some(Keyword(_,v)) => res.withColumn("_source",lit(v))
-          case _=> res
-        }
-        res = keywordsMap.get("sourcetype") match {
-          case Some(Keyword(_,v)) => res.withColumn("_sourcetype",lit(v))
-          case _=> res
-        }
-        res = if(!isTimed) res.withColumn("_time",lit(OtDatetime.getCurrentTimeInSeconds())) else res
-        val expr = res.schema.fields.foldLeft(List[Column]()){ (accum, item) => lit(item.name) :: lit("=") :: col(item.name) :: (lit(",") :: accum) }.dropRight(1)
-
-        res = if(true) res.withColumn("_raw", concat(expr: _* )) else res//!hasRaw
-        if (log.getLevel == Level.DEBUG) {
-          log.debug(f"[SearchId:${sq.searchId}] Output:\n" + StatViewer.getPreviewString(res))
+        val res = keywordsMap.get("mode") match {
+          case Some(Keyword(_, "as-is")) => _df
+          case _ =>
+            var raw_df = keywordsMap.get("host") match {
+              case Some(Keyword(_, v)) => _df.withColumn("_host", lit(v))
+              case _ => _df
+            }
+            raw_df = keywordsMap.get("source") match {
+              case Some(Keyword(_, v)) => raw_df.withColumn("_source", lit(v))
+              case _ => raw_df
+            }
+            raw_df = keywordsMap.get("sourcetype") match {
+              case Some(Keyword(_, v)) => raw_df.withColumn("_sourcetype", lit(v))
+              case _ => raw_df
+            }
+            raw_df = if (!isTimed) raw_df.withColumn("_time", lit(OtDatetime.getCurrentTimeInSeconds())) else raw_df
+            val expr = raw_df.schema.fields.foldLeft(List[Column]()) { (accum, item) => lit(item.name) :: lit("=") :: col(item.name) :: (lit(",") :: accum) }.dropRight(1)
+            raw_df.withColumn("_raw", concat(expr: _*))
         }
         res.write.parquet(f"$fsdisk$indexPathDisk/$i/bucket-${timeMin*1000}-${timeMax*1000}-${System.currentTimeMillis / 1000}")
         res
+
       case _ =>
         log.error("Required argument 'index' not found")
         throw new CustomException(4, sq.searchId, "Required argument 'index' not found",List(commandname, "index"))
-        //_df
     }
   }
 }
