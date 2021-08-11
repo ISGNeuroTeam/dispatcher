6045cf47be3d2d3a6ad679179ffe1692a1b3f775
Федор Фастовский <fedorfastovskij@Fedors-MacBook-Pro.local>
2021-08-11 20:36:07 +0300

    error on commands : ... | fieldsummary | table ... , fix

--- src/main/scala/ot/scalaotl/commands/OTLFieldSummary.scala
+++ src/main/scala/ot/scalaotl/commands/OTLFieldSummary.scala
@@ -13,17 +13,17 @@ class OTLFieldSummary(sq: SimpleQuery) extends OTLBaseCommand(sq) {
 
   override def transform(_df: DataFrame): DataFrame = {
 
+    val describedDF = _df.describe()
     val columnName = getKeyword("column_name").getOrElse("column")
+    val columns = describedDF.columns
 
-    val columns = _df.columns
-
-    _df.describe()
+    describedDF
       .select(col("summary"), zipUdf(lit(columns), array(columns.map(col): _*)).as("temp"))
       .withColumn("rows", explode(col("temp")))
       .select(col("summary"), col("rows._1").as(columnName), col("rows._2").as("value"))
       .groupBy(columnName)
       .pivot("summary")
       .agg(first("value"))
-      //.toDF(headers: _*)
+      .where(!(col(columnName) === "summary"))
   }
 }
