dfbbeffeaaee57e57ea4a4ee734d70523315626a
ffastovskii <ffastovskii@isgneuro.com>
2021-09-29 16:36:54 +0300

    CacheManager can read any spark type

--- src/main/scala/ot/dispatcher/CacheManager.scala
+++ src/main/scala/ot/dispatcher/CacheManager.scala
@@ -1,6 +1,6 @@
 package ot.dispatcher
 
-import org.apache.spark.sql.types.{DoubleType, IntegerType, LongType, NullType, StringType, StructField, StructType, ArrayType, BooleanType}
+import org.apache.spark.sql.types.{ArrayType, BooleanType, DoubleType, IntegerType, LongType, NullType, StringType, StructField, StructType}
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import ot.AppConfig._
@@ -8,38 +8,39 @@ import ot.dispatcher.sdk.core.CustomException.{E00007, E00011}
 import ot.dispatcher.sdk.core.CustomException
 
 import scala.reflect.io.File
+import scala.util.{Failure, Success, Try}
 
 /** Makes all manipulations with Jobs result caches.
-  * [[makeCache]] - Limits and saves result [[DataFrame]] to RAM cache.
-  * [[removeCache]] - Remove cache files from RAM cache.
-  * [[loadCache]] - Loads [[DataFrame]] from RAM cache.
-  *
-  * @param sparkSession [[SparkSession]] for loading cache.
-  * @author Andrey Starchenkov (astarchenkov@ot.ru)
-  */
+ * [[makeCache]] - Limits and saves result [[DataFrame]] to RAM cache.
+ * [[removeCache]] - Remove cache files from RAM cache.
+ * [[loadCache]] - Loads [[DataFrame]] from RAM cache.
+ *
+ * @param sparkSession [[SparkSession]] for loading cache.
+ * @author Andrey Starchenkov (astarchenkov@ot.ru)
+ */
 class CacheManager(sparkSession: SparkSession) {
 
   val log: Logger = Logger.getLogger("CacheManagerLogger")
-  log.setLevel(Level.toLevel(getLogLevel(config,"cachemanager")))
+  log.setLevel(Level.toLevel(getLogLevel(config, "cachemanager")))
 
   // Loads settings for RAM cache from config.
   val fs: String = config.getString("memcache.fs")
   val path: String = config.getString("memcache.path")
-  val maxRows = config.getInt("indexes.max_rows")
+  val maxRows: Int = config.getInt("indexes.max_rows")
 
   /** Saves result [[DataFrame]] to RAM cache.
-    * Before saving sets format and limits to cache, writes the scheme of DF.
-    *
-    * @param df Resulting [[DataFrame]].
-    * @param id Job ID.
-    */
+   * Before saving sets format and limits to cache, writes the scheme of DF.
+   *
+   * @param df Resulting [[DataFrame]].
+   * @param id Job ID.
+   */
   def makeCache(df: DataFrame, id: Int): Unit = {
     log.debug(s"Job $id. Cache: $fs$path" + s"search_$id.cache. Schema: ${df.schema}.")
     try {
       df.limit(maxRows).write
         .format("json")
         .save(s"$fs$path" + s"search_$id.cache/data")
-    }catch {
+    } catch {
       case ex: Exception => throw makeCustomException(ex, id)
     }
 
@@ -47,7 +48,7 @@ class CacheManager(sparkSession: SparkSession) {
     log.debug(s"Job $id. Cache: $fs$path" + s"search_$id.cache is written.")
   }
 
-  private def makeCustomException(ex: Exception, id: Int)= {
+  private def makeCustomException(ex: Exception, id: Int) = {
     val exception = ex match {
       case e0 if !e0.getMessage.equals("Job aborted.") => e0
       case e0 => e0.getCause match {
@@ -58,14 +59,14 @@ class CacheManager(sparkSession: SparkSession) {
         }
       }
     }
-    log.error(f"Runtime error: ${exception.getMessage}" )
+    log.error(f"Runtime error: ${exception.getMessage}")
     E00007(id, exception.getMessage, exception)
   }
 
   /** Removes cache files from RAM cache.
-    *
-    * @param id Cache ID.
-    */
+   *
+   * @param id Cache ID.
+   */
   def removeCache(id: Int): Unit = {
     import scala.reflect.io.File
     val pathToCache = s"$path" + s"search_$id.cache/"
@@ -76,17 +77,41 @@ class CacheManager(sparkSession: SparkSession) {
   }
 
   /** Loads [[DataFrame]] from RAM cache.
-    * Before loading sets format of saved data and checks if header is present.
-    *
-    * @param id Cache ID.
-    * @return [[DataFrame]] with cache data.
-    */
+   * Before loading sets format of saved data and checks if header is present.
+   *
+   * @param id Cache ID.
+   * @return [[DataFrame]] with cache data.
+   */
   def loadCache(id: Int): DataFrame = {
 
     val schema = File(s"${path}search_$id.cache/data/_SCHEMA").bufferedReader().readLine()
+    val pattern = "`([^`]+)` ([A-Za-z]+(\\([0-9]+,[0-9]+\\))?)".r //group1 - field name, group2 - field type, group3 - precision
 
-    val structType = StructType.fromDDL(schema)
-    
+    val structFields = schema.split(",\\s*(?![^()]*\\))").map { //split by comma without comma in parentheses
+      structFieldDDLString =>
+        Try {
+          StructType.fromDDL(structFieldDDLString)
+        } match {
+          case Success(structType) => structType.fields.head
+          case Failure(exception) =>
+            log.debug(s"Caught exception '${exception.getMessage}' while parsing SCHEMA fields")
+
+            val structFieldMatch = pattern.findFirstMatchIn(structFieldDDLString)
+            structFieldMatch match {
+              case Some(fieldTypeMatch) =>
+                val fieldName = fieldTypeMatch.group(1)
+                val fieldType = fieldTypeMatch.group(2)
+
+                if (fieldType == "NULL") StructField(fieldName, NullType, nullable = true) // StructType.fromDDL do not support NULL type
+                else throw exception
+
+              case _ => throw exception
+            }
+        }
+    }
+
+    val structType = StructType(structFields)
+
     log.debug(s"Subsearch $id schema: $schema.")
     val df = sparkSession.read
       .format("json")
