f939eea8885ddd88d8229e328bd5d30d344d71f5
DNikolaevKlg <dnikolaev@isgneuro.com>
2022-04-25 15:31:07 +0300

    OTL command top normalization

--- src/main/scala/ot/scalaotl/commands/OTLTop.scala
+++ src/main/scala/ot/scalaotl/commands/OTLTop.scala
@@ -6,6 +6,69 @@ import org.apache.spark.sql.expressions.Window
 import org.apache.spark.sql.functions._
 import ot.scalaotl.extensions.StringExt._
 
+/** =Abstract=
+ * This class provides support of __'''top'''__ otl command.
+ *
+ * __'''top'''__ finds the most frequently occurring values in the selected fields of event,
+ *                calculates the number and percentage of how often the selected values occur in events.
+ *
+ * __'''top'''__ takes one required and two optional arguments:
+ *
+ * Required argument:
+ *
+ *    1.  _'''field-list'''_ - comma-separated field names to which the command will apply.
+ *
+ * Optional arguments:
+ *
+ *    1. __'''number'''__ - the number of top rows to be output in the result. The default is 10.
+ *
+ *    2.__'''by'''__ - defines the field by which grouping occurs in the table.
+ *
+ * =Usage examples=
+ * * OTL 1:
+ * {{{| makeresults | eval a = 10,b=200 | append [makeresults | eval a =20,b=300] | append [makeresults | eval a =40,b=400]
+ * | append [makeresults | eval a =30,b=400] | append [makeresults | eval a =20,b=700] | append [makeresults | eval a =10,b=300]
+ * | append [makeresults | eval a =30,b=200] | append [makeresults | eval a =10,b=500] | top a}}}
+ * Result:
+ * {{{+------+--------+
+|    a| count| percent|
++---  +------+--------+
+|   10|     3|    37.5|
+|   20|     2|      25|
+|   30|     2|      25|
+|   40|     1|    12.5|
++-----+------+--------+}}}
+ * OTL 2:
+ * {{{| makeresults | eval a = 10,b=200 | append [makeresults | eval a =20,b=300] | append [makeresults | eval a =40,b=400]
+ * | append [makeresults | eval a =30,b=400] | append [makeresults | eval a =20,b=700] | append [makeresults | eval a =10,b=300]
+ * | append [makeresults | eval a =30,b=200] | append [makeresults | eval a =10,b=500] | top 2 a}}}
+ * Result:
+ * {{{+------+--------+
+|    a| count| percent|
++---  +------+--------+
+|   10|     3|    37.5|
+|   20|     2|      25|
++-----+------+--------+}}}
+ * OTL 3:
+ * {{{| makeresults | eval a = 10,b=200 | append [makeresults | eval a =20,b=300] | append [makeresults | eval a =40,b=400]
+ * | append [makeresults | eval a =30,b=400] | append [makeresults | eval a =20,b=700] | append [makeresults | eval a =10,b=300]
+ * | append [makeresults | eval a =30,b=200] | append [makeresults | eval a =10,b=500] | top a b}}}
+ * Result:
+ * {{{+---+------+--------+
+|    a|  b| count| percent|
++---  +---+------+--------+
+|   40|400|     1|    12.5|
+|   30|200|     1|    12.5|
+|   30|400|     1|    12.5|
+|   10|300|     1|    12.5|
+|   10|200|     1|    12.5|
+|   10|500|     1|    12.5|
+|   20|300|     1|    12.5|
+|   20|700|     1|    12.5|
++-----+---+------+--------+}}}
+ * @constructor creates new instance of [[OTLTop]]
+ * @param sq [[SimpleQuery]]
+ */
 class OTLTop(sq: SimpleQuery) extends OTLBaseCommand(sq, _seps = Set("by")) {
   val requiredKeywords = Set.empty[String]
   val optionalKeywords = Set.empty[String]
