573d260ca966dfad7521e1c5b7c72fbfa8f1266b
nizam <nkeramov@isgneuro.ru>
2022-06-28 19:35:54 +0500

    updated docstrings and tests

--- src/main/scala/ot/scalaotl/commands/OTLTransaction.scala
+++ src/main/scala/ot/scalaotl/commands/OTLTransaction.scala
@@ -8,7 +8,7 @@ import org.apache.spark.sql.functions.{array, array_min, col, collect_set, expr,
 /** =Abstract=
  * This class provides support of __'''transaction'''__ otl command.
  *
- * __'''transaction'''__ used to convert wide table data to a long table.
+ * __'''transaction'''__ used to finds first record with unique set of specified fields.
  * The command allows to find among the many records grouped by the specified set of fields, find the first record by time.
  * The specified set of fields is called a transaction
  *
@@ -42,20 +42,26 @@ class OTLTransaction(sq: SimpleQuery) extends OTLBaseCommand(sq) {
   def convertArraysToSingle(df: DataFrame): DataFrame = {
     val sch = df.schema
     val colsInit = df.columns.filter(sch(_).dataType.typeName == "array")
-    val funcs = colsInit.map(x => collect_set(size(col(x))).alias(x))
-    val dfLen = colsInit.foldLeft(df.agg(funcs.head, funcs.tail: _*)) {
-      case (accum, colname) => accum.withColumn(colname, size(col(colname)))
+    // if df not contains array type columns then return df
+    if (colsInit.nonEmpty) {
+      val funcs = colsInit.map(x => collect_set(size(col(x))).alias(x))
+      val dfLen = colsInit.foldLeft(df.agg(funcs.head, funcs.tail: _*)) {
+        case (accum, colname) => accum.withColumn(colname, size(col(colname)))
+      }
+      val mapCols = createMap(dfLen).select("total").collect.head.get(0).asInstanceOf[Map[String, Int]]
+      colsInit.foldLeft(df) {
+        case (accum, item) => if (mapCols(item).toString == "1") accum.withColumn(item, col(item)(0)) else accum
+      }
     }
-    val mapCols = createMap(dfLen).select("total").collect.head.get(0).asInstanceOf[Map[String, Int]]
-    colsInit.foldLeft(df) {
-      case (accum, item) => if (mapCols(item).toString == "1") accum.withColumn(item, col(item)(0)) else accum
-    }
+    else
+      df
   }
 
   override def transform(_df: DataFrame): DataFrame = {
     // calling OTL-stats with received arguments
     val s = SimpleQuery(s"values(*) as * by $args")
     val dfTransaction = convertArraysToSingle(new OTLStats(s).transform(_df))
+    // if _time not exists in original df then return empty dataframe
     if (dfTransaction.columns.contains("_time")){
       if (dfTransaction.schema("_time").dataType.typeName == "array")
         dfTransaction.withColumn("_time", array_min(col("_time")))
--- src/test/scala/ot/scalaotl/commands/OTLTransactionTest.scala
+++ src/test/scala/ot/scalaotl/commands/OTLTransactionTest.scala
@@ -5,8 +5,25 @@ import ot.scalaotl.Converter
 
 class OTLTransactionTest extends CommandTest {
 
-  // вообще не работает
-  test("Test 1. Command: Dataset with _time, run transaction random_Field, WordField") {
+
+  override val dataset: String =
+    """[
+      |{"_time":1568026476854,"_meta":"_subsecond::.854 timestamp::none","host":"test.local:9990","sourcetype":"jmx","index":"test_index","source":"test_source","_raw":"{\"serialField\": \"0\", \"random_Field\": \"100\", \"WordField\": \"qwe\", \"junkField\": \"q2W\"}","_nifi_time":"1568037188486","serialField":"0","random_Field":"100","WordField":"qwe","junkField":"q2W","_subsecond":"854","timestamp":"none","_nifi_time_out":"1568037488751"},
+      |{"_time":1568026476854,"_meta":"_subsecond::.854 timestamp::none","host":"test.local:9990","sourcetype":"jmx","index":"test_index","source":"test_source","_raw":"{\"serialField\": \"1\", \"random_Field\": \"-90\", \"WordField\": \"rty\", \"junkField\": \"132_.\"}","_nifi_time":"1568037188487","serialField":"1","random_Field":"-90","WordField":"rty","junkField":"132_.","_subsecond":"854","timestamp":"none","_nifi_time_out":"1568037488752"},
+      |{"_time":1568026476854,"_meta":"_subsecond::.854 timestamp::none","host":"test.local:9990","sourcetype":"jmx","index":"test_index","source":"test_source","_raw":"{\"serialField\": \"2\", \"random_Field\": \"50\", \"WordField\": \"uio\", \"junkField\": \"asd.cx\"}","_nifi_time":"1568037188487","serialField":"2","random_Field":"50","WordField":"uio","junkField":"asd.cx","_subsecond":"854","timestamp":"none","_nifi_time_out":"1568037482371"},
+      |{"_time":1568026471894,"_meta":"_subsecond::.854 timestamp::none","host":"test.local:9990","sourcetype":"jmx","index":"test_index","source":"test_source","_raw":"{\"serialField\": \"2\", \"random_Field\": \"50\", \"WordField\": \"uio\", \"junkField\": \"asd.cx\"}","_nifi_time":"1568037188487","serialField":"2","random_Field":"50","WordField":"uio","junkField":"asd.cx","_subsecond":"854","timestamp":"none","_nifi_time_out":"1568037488752"},
+      |{"_time":1568026476854,"_meta":"_subsecond::.854 timestamp::none","host":"test.local:9990","sourcetype":"jmx","index":"test_index","source":"test_source","_raw":"{\"serialField\": \"3\", \"random_Field\": \"20\", \"WordField\": \"GreenPeace\", \"junkField\": \"XYZ\"}","_nifi_time":"1568037188487","serialField":"3","random_Field":"20","WordField":"GreenPeace","junkField":"XYZ","_subsecond":"854","timestamp":"none","_nifi_time_out":"1568037488752"},
+      |{"_time":1568026476854,"_meta":"_subsecond::.854 timestamp::none","host":"test.local:9990","sourcetype":"jmx","index":"test_index","source":"test_source","_raw":"{\"serialField\": \"4\", \"random_Field\": \"30\", \"WordField\": \"fgh\", \"junkField\": \"123_ASD\"}","_nifi_time":"1568037188487","serialField":"4","random_Field":"30","WordField":"fgh","junkField":"123_ASD","_subsecond":"854","timestamp":"none","_nifi_time_out":"1568037488752"},
+      |{"_time":1568024476951,"_meta":"_subsecond::.854 timestamp::none","host":"test.local:9990","sourcetype":"jmx","index":"test_index","source":"test_source","_raw":"{\"serialField\": \"4\", \"random_Field\": \"30\", \"WordField\": \"fgh\", \"junkField\": \"123_ASD\"}","_nifi_time":"1568037188487","serialField":"4","random_Field":"30","WordField":"fgh","junkField":"123_ASD","_subsecond":"854","timestamp":"none","_nifi_time_out":"1568037488272"},
+      |{"_time":1568026476854,"_meta":"_subsecond::.854 timestamp::none","host":"test.local:9990","sourcetype":"jmx","index":"test_index","source":"test_source","_raw":"{\"serialField\": \"5\", \"random_Field\": \"50\", \"WordField\": \"jkl\", \"junkField\": \"casd(@#)asd\"}","_nifi_time":"1568037188487","serialField":"5","random_Field":"50","WordField":"jkl","junkField":"casd(@#)asd","_subsecond":"854","timestamp":"none","_nifi_time_out":"1568037488752"},
+      |{"_time":1568026472834,"_meta":"_subsecond::.854 timestamp::none","host":"test.local:9990","sourcetype":"jmx","index":"test_index","source":"test_source","_raw":"{\"serialField\": \"5\", \"random_Field\": \"50\", \"WordField\": \"jkl\", \"junkField\": \"casd(@#)asd\"}","_nifi_time":"1568037188487","serialField":"5","random_Field":"50","WordField":"jkl","junkField":"casd(@#)asd","_subsecond":"854","timestamp":"none","_nifi_time_out":"1568037488322"},
+      |{"_time":1568026476854,"_meta":"_subsecond::.854 timestamp::none","host":"test.local:9990","sourcetype":"jmx","index":"test_index","source":"test_source","_raw":"{\"serialField\": \"6\", \"random_Field\": \"60\", \"WordField\": \"zxc\", \"junkField\": \"QQQ.2\"}","_nifi_time":"1568037188487","serialField":"6","random_Field":"60","WordField":"zxc","junkField":"QQQ.2","_subsecond":"854","timestamp":"none","_nifi_time_out":"1568037488752"},
+      |{"_time":1568026476854,"_meta":"_subsecond::.854 timestamp::none","host":"test.local:9990","sourcetype":"jmx","index":"test_index","source":"test_source","_raw":"{\"serialField\": \"7\", \"random_Field\": \"-100\", \"WordField\": \"RUS\", \"junkField\": \"00_3\"}","_nifi_time":"1568037188487","serialField":"7","random_Field":"-100","WordField":"RUS","junkField":"00_3","_subsecond":"854","timestamp":"none","_nifi_time_out":"1568037488752"},
+      |{"_time":1568026476854,"_meta":"_subsecond::.854 timestamp::none","host":"test.local:9990","sourcetype":"jmx","index":"test_index","source":"test_source","_raw":"{\"serialField\": \"8\", \"random_Field\": \"0\", \"WordField\": \"MMM\", \"junkField\": \"112\"}","_nifi_time":"1568037188487","serialField":"8","random_Field":"0","WordField":"MMM","junkField":"112","_subsecond":"854","timestamp":"none","_nifi_time_out":"1568037488752"},
+      |{"_time":1568026476854,"_meta":"_subsecond::.854 timestamp::none","host":"test.local:9990","sourcetype":"jmx","index":"test_index","source":"test_source","_raw":"{\"serialField\": \"9\", \"random_Field\": \"10\", \"WordField\": \"USA\", \"junkField\": \"word\"}","_nifi_time":"1568037188487","serialField":"9","random_Field":"10","WordField":"USA","junkField":"word","_subsecond":"854","timestamp":"none","_nifi_time_out":"1568037488752"}
+      |]"""
+
+  test("Test 1. Command: Dataset with _time, args includes not all fields") {
     val query = createQuery("""table _time, random_Field, WordField | transaction random_Field, WordField""",
       "otstats", s"$test_index")
     var actual = new Converter(query).run
@@ -17,9 +34,9 @@ class OTLTransactionTest extends CommandTest {
         |{"random_Field":"20","WordField":"GreenPeace","_time":1568026476854},
         |{"random_Field":"0","WordField":"MMM","_time":1568026476854},
         |{"random_Field":"100","WordField":"qwe","_time":1568026476854},
-        |{"random_Field":"50","WordField":"uio","_time":1568026476854},
-        |{"random_Field":"50","WordField":"jkl","_time":1568026476854},
-        |{"random_Field":"30","WordField":"fgh","_time":1568026476854},
+        |{"random_Field":"50","WordField":"uio","_time":1568026471894},
+        |{"random_Field":"50","WordField":"jkl","_time":1568026472834},
+        |{"random_Field":"30","WordField":"fgh","_time":1568024476951},
         |{"random_Field":"10","WordField":"USA","_time":1568026476854},
         |{"random_Field":"-90","WordField":"rty","_time":1568026476854},
         |{"random_Field":"60","WordField":"zxc","_time":1568026476854}
@@ -27,13 +44,43 @@ class OTLTransactionTest extends CommandTest {
     compareDataFrames(actual, jsonToDf(expected))
   }
 
-  test("Test 2. Command: Dataset without _time, run transaction random_Field, WordField") {
-    val query = createQuery("""table random_Field, WordField | transaction random_Field, WordField""",
+  test("Test 2. Command: Dataset with _time, args includes all fields") {
+    val query = createQuery("""table _time, random_Field, WordField | transaction _time, random_Field, WordField""",
       "otstats", s"$test_index")
-    val actual = new Converter(query).run
+    var actual = new Converter(query).run
+    actual = actual.select(actual.columns.sorted.toSeq.map(c => col(c)):_*)
     val expected =
       """[
+        |{"_time":1568026476854,"random_Field":"50","WordField":"jkl"},
+        |{"_time":1568026476854,"random_Field":"10","WordField":"USA"},
+        |{"_time":1568026476854,"random_Field":"50","WordField":"uio"},
+        |{"_time":1568026476854,"random_Field":"100","WordField":"qwe"},
+        |{"_time":1568026476854,"random_Field":"20","WordField":"GreenPeace"},
+        |{"_time":1568026476854,"random_Field":"30","WordField":"fgh"},
+        |{"_time":1568026476854,"random_Field":"-90","WordField":"rty"},
+        |{"_time":1568026471894,"random_Field":"50","WordField":"uio"},
+        |{"_time":1568026472834,"random_Field":"50","WordField":"jkl"},
+        |{"_time":1568026476854,"random_Field":"-100","WordField":"RUS"},
+        |{"_time":1568026476854,"random_Field":"0","WordField":"MMM"},
+        |{"_time":1568024476951,"random_Field":"30","WordField":"fgh"},
+        |{"_time":1568026476854,"random_Field":"60","WordField":"zxc"}
         |]""".stripMargin
     compareDataFrames(actual, jsonToDf(expected))
   }
+
+  test("Test 3. Command: Dataset without _time, args includes not all fields") {
+    val query = createQuery("""table WordField, random_Field, junkField, serialField | transaction random_Field, WordField""",
+      "otstats", s"$test_index")
+    val actual = new Converter(query).run
+    compareDataFrames(actual, spark.emptyDataFrame)
+  }
+
+  test("Test 4. Command: Dataset without _time, args includes all fields") {
+    val query = createQuery("""table WordField, random_Field, junkField, serialField | transaction WordField, random_Field, junkField, serialField""",
+      "otstats", s"$test_index")
+    val actual = new Converter(query).run
+    compareDataFrames(actual, spark.emptyDataFrame)
+  }
+
+
 }
