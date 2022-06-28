1fce2a6bf16539ca1a281dc83f5c674fdcaafbc1
nizam <nkeramov@isgneuro.ru>
2022-06-28 16:26:00 +0500

    otl-untable added docstrings, updated tests

--- src/main/scala/ot/scalaotl/commands/OTLUntable.scala
+++ src/main/scala/ot/scalaotl/commands/OTLUntable.scala
@@ -5,29 +5,52 @@ import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.functions.{col, expr, explode}
 import ot.scalaotl.extensions.StringExt._
 
+/** =Abstract=
+ * This class provides support of __'''untable'''__ otl command.
+ *
+ * __'''untable'''__ used to convert wide table data to a long table.
+ * The command allows, having fixed the value of one of the columns (for example, the _time field),
+ * to split each of the lines of the source table into several lines.
+ *
+ * Command syntax untable <fixed_column_name>, <field_column_name>, <value_column_name>
+ * First column name is fixed column, second is name of column with metrics names and third is column with metrics values.
+ * The first column must be present in the dataset.
+ *
+ * =Usage example=
+ * OTL: keep only selected fields
+ * {{{  other otl-commands ... | untable _time, metric_name, value }}}
+ *
+ * @constructor creates new instance of [[ OTLUntable ]]
+ * @param sq [[ SimpleQuery ]] - contains args, cache, subsearches, search time interval, stfe and preview flags
+ */
 class OTLUntable(sq: SimpleQuery) extends OTLBaseCommand(sq) {
   val requiredKeywords = Set.empty[String]
   val optionalKeywords = Set.empty[String]
 
   override def transform(_df: DataFrame): DataFrame = {
-    _df.printSchema()
+    // get fixed, field and value column names
     val (fixed, field, value) = returns.flatFields match {
       case x :: y :: z :: tail => (
         x.stripBackticks().strip("\""),
         y.stripBackticks().strip("\""),
         z.stripBackticks().strip("\"")
       )
-      case _ => return _df
+      case _ => return spark.emptyDataFrame
     }
+    // select columns that not in args
     val cols = _df.columns.filter(x => x != fixed && x != field && x != value).map(_.addSurroundedBackticks)
+    // for all columns that not in args convert to arrays [column name, value]
     cols.foldLeft(_df) {
       (accum, colname) => {
         accum.withColumn(colname.stripBackticks(), expr(s"""array("${colname.stripBackticks()}", $colname)"""))
       }
     }
+      // adding column arr as an array containing all other columns
       .withColumn("arr", expr(s"""array(${cols.mkString(", ")})"""))
       .select(fixed.strip("\""), "arr")
+      // explode arr column to separate arrays [column name, value]
       .withColumn("arr", explode(col("arr")))
+      // create field and value columns from created arrays
       .withColumn(field.strip("\""), col("arr").getItem(0))
       .withColumn(value.strip("\""), col("arr").getItem(1))
       .drop("arr")
