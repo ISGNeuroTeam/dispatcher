a59681c4411489a8c27cc31a9431b559e4839601
nizam <nkeramov@isgneuro.ru>
2022-07-07 13:27:56 +0500

    updated otl-return and otl-return tests

--- src/main/scala/ot/scalaotl/commands/OTLReturn.scala
+++ src/main/scala/ot/scalaotl/commands/OTLReturn.scala
@@ -6,6 +6,37 @@ import org.apache.spark.sql.DataFrame
 import ot.scalaotl.extensions.StringExt._
 import org.apache.spark.sql.functions._
 
+/** =Abstract=
+ * This class provides support of __'''return'''__ otl command.
+ *
+ * __'''return'''__ used to keeps or remove the selected fields from the query results.
+ * If the command is placed at the end of the request, then the indicated fields will be displayed on the screen.
+ * If the command is specified in the middle of the query, then further data processing will occur with the fields
+ * specified after fields.
+ * Fields is a synonym for the command table.
+ *
+ * Command syntax return [count] <field-list>
+ *
+ * If "-" is specified, then the fields from <field-list> will be removed from the query results.
+ * If an asterisk "*" is specified, then all fields will be retrieved.
+ * Field names can use wildcards.
+ *
+ * =Usage example=
+ * OTL: keep only selected fields
+ * {{{  other otl-commands ... | fields metric_name, value }}}
+ *
+ * OTL: remove selected fields
+ * {{{  other otl-commands ... | fields - metric_long_name, _raw }}}
+ *
+ * OTL: keep selected fields with wildcards
+ * {{{  other otl-commands ... | fields _*, metric*, value }}}
+ *
+ * OTL: get all fields from index
+ * {{{  other otl-commands ... | fields * }}}
+ *
+ * @constructor creates new instance of [[ OTLReturn ]]
+ * @param sq [[ SimpleQuery ]] - contains args, cache, subsearches, search time interval, stfe and preview flags
+ */
 class OTLReturn(sq: SimpleQuery) extends OTLBaseCommand(sq) {
   val requiredKeywords = Set.empty[String]
   val optionalKeywords = Set.empty[String]
@@ -21,6 +52,12 @@ class OTLReturn(sq: SimpleQuery) extends OTLBaseCommand(sq) {
 
   val fields: List[String] = returns.flatFields.filter(_ != count.toString.addSurroundedBackticks)
 
+  /**
+   * Standard method called by Converter in each OTL-command.
+   *
+   * @param _df [[DataFrame]] - incoming dataset (in generator-command like this one is ignored and should be empty)
+   * @return [[DataFrame]]  - outgoing dataframe with OTL-command results
+   */
   override def transform(_df: DataFrame): DataFrame = {
     val nFields = fields.map(_.stripBackticks()).filter(_.startsWith("$"))
     val ndf = nFields.foldLeft(_df)((acc, f) => acc.withColumn(f, col(f.stripPrefix("$"))))
--- src/test/scala/ot/scalaotl/commands/OTLReturnTest.scala
+++ src/test/scala/ot/scalaotl/commands/OTLReturnTest.scala
@@ -1,38 +1,76 @@
 package ot.scalaotl.commands
 
-import org.scalatest.Ignore
+import org.apache.spark.sql.functions.col
+import org.apache.spark.sql.types.{DoubleType, IntegerType}
+import org.apache.spark.sql.{Column, DataFrame, functions => F}
+import ot.scalaotl.Converter
 
-@Ignore
 class OTLReturnTest extends CommandTest {
 
-//  test("Test 0. Command: | return serialField ") {
-//    val actual = execute(""" table serialField | return 5 serialField """)
-//    val expected = """[
-//                     |{"search":"(serialField=\"0\") OR (serialField=\"1\") OR (serialField=\"2\") OR (serialField=\"3\") OR (serialField=\"4\")"}
-//                     |]""".stripMargin
-//    assert(jsonCompare(actual, expected), f"Result : $actual\n---\nExpected : $expected")
-//  }
-//
-//  test("Test 1. Command: | return 1 serialField WordField ") {
-//    val actual = execute(""" table serialField WordField | return serialField WordField""")
-//    val expected = """[
-//                     |{"search":"serialField=\"0\" WordField=\"qwe\""}
-//                     |]""".stripMargin
-//    assert(jsonCompare(actual, expected), f"Result : $actual\n---\nExpected : $expected")
-//  }
-//
-//  test("Test 2. Command: | return serialField WordField") {
-//    val actual = execute(""" table serialField WordField | return 5 serialField WordField""")
-//    val expected = """[
-//                     |{"search":"(serialField=\"0\" WordField=\"qwe\") OR (serialField=\"1\" WordField=\"rty\") OR (serialField=\"2\" WordField=\"uio\") OR (serialField=\"3\" WordField=\"GreenPeace\") OR (serialField=\"4\" WordField=\"fgh\")"}
-//                     |]""".stripMargin
-//    assert(jsonCompare(actual, expected), f"Result : $actual\n---\nExpected : $expected")
-//  }
-//  test("Test 3. Command: | return $serialField WordField") {
-//    val actual = execute("""  return 5 $serialField WordField""")
-//    val expected = """[
-//                     |{"search":"(0 WordField=\"qwe\") OR (1 WordField=\"rty\") OR (2 WordField=\"uio\") OR (3 WordField=\"GreenPeace\") OR (4 WordField=\"fgh\")"}
-//                     |]""".stripMargin
-//    assert(jsonCompare(actual, expected), f"Result : $actual\n---\nExpected : $expected")
-//  }
+  test("Test 1. Command: | Return value from subsearch") {
+    val ssQuery = createQuery(s"sort -serialField | return $$serialField",
+      "otstats", s"$test_index")
+    val dfc = new Converter(ssQuery).run
+    val cache = Map("id1" -> dfc)
+    val mainQuery = createQuery("""table _time, serialField | eval max_serialField=subsearch=id1""",
+      "otstats", s"$test_index")
+    var actual = new Converter(mainQuery, cache).run
+    actual = setNullableStateOfColumn(actual, "max_serialField", nullable = true)
+    val expected = readIndexDF(test_index)
+      .select(F.col("_time"), F.col("serialField"))
+      .withColumn("max_serialField", (F.max("serialField") over ()))
+      .withColumn("max_serialField", col("max_serialField").cast(IntegerType))
+    compareDataFrames(actual, expected)
+  }
+
+  test("Test 2. Command: | Return value from subsearch with wrong field name ") {
+    val ssQuery = createQuery(s"sort -serialField | return $$serial_Field",
+      "otstats", s"$test_index")
+    val dfc = new Converter(ssQuery).run
+    val cache = Map("id1" -> dfc)
+    val mainQuery = createQuery("""table _time, serialField | eval max_serialField=subsearch=id1""",
+      "otstats", s"$test_index")
+    val thrown = intercept[Exception] {
+      new Converter(mainQuery, cache).run
+    }
+    assert(thrown.getMessage.endsWith("Command eval shoud have at least one argument"))
+  }
+
+  test("Test 3. Command: | return expression for eval") {
+    val ssQuery = createQuery(s"""eval val= "value = tonumber(serialField) * tonumber(random_Field)" | return $$val""",
+      "otstats", s"$test_index")
+    val dfc = new Converter(ssQuery).run
+    val cache = Map("id1" -> dfc)
+    val mainQuery = createQuery("""table _time, serialField, random_Field | eval subsearch=id1""",
+      "otstats", s"$test_index")
+    var actual = new Converter(mainQuery, cache).run
+    actual = setNullableStateOfColumn(actual, "value", nullable = true)
+    actual.show(10, false)
+    actual.printSchema()
+    val expected = readIndexDF(test_index)
+      .select(F.col("_time"), F.col("serialField"), F.col("random_Field"))
+      .withColumn("value", F.col("serialField").cast(DoubleType) * F.col("random_Field").cast(DoubleType))
+    compareDataFrames(actual, expected)
+  }
+
+
+  test("Test 4. Command: | Return more than one value from subsearch") {
+    val ssQuery = createQuery(s"sort -serialField | return count=5 $$serialField",
+      "otstats", s"$test_index")
+    val dfc = new Converter(ssQuery).run
+    dfc.printSchema()
+    dfc.show()
+    val cache = Map("id1" -> dfc)
+    val mainQuery = createQuery("""table _time, serialField | where serialField=subsearch=id1""",
+      "otstats", s"$test_index")
+    var actual = new Converter(mainQuery, cache).run
+    actual = setNullableStateOfColumn(actual, "serialField", nullable = true)
+    actual.printSchema()
+    actual.show(10, false)
+    val expected = readIndexDF(test_index)
+      .select(F.col("_time"), F.col("serialField"))
+      .withColumn("max_serialField", (F.max("serialField") over ()))
+      .withColumn("max_serialField", col("max_serialField").cast(IntegerType))
+    compareDataFrames(actual, expected)
+  }
 }
