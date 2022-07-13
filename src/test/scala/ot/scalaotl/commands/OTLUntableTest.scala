957eac848c2cb715f38351e7a0cab57a513f86b1
nizam <nkeramov@isgneuro.ru>
2022-07-13 18:23:49 +0500

    Revert "Merge pull request #8 from ISGNeuroTeam/feature/otl-untable-normalization"
    This reverts commit 69dad93045d441e20a3f7265520f54a7f051f571, reversing
    changes made to a31cfd02d531ea118e8c76e749a2582fb2f3378c.

--- src/test/scala/ot/scalaotl/commands/OTLUntableTest.scala
+++ src/test/scala/ot/scalaotl/commands/OTLUntableTest.scala
@@ -1,111 +1,34 @@
 package ot.scalaotl.commands
 
-import org.apache.spark.sql.functions.col
-import ot.scalaotl.Converter
-import org.apache.spark.sql.{Column, DataFrame, SparkSession, functions => F}
-
 class OTLUntableTest extends CommandTest {
 
-  test("Test 1. Command: Convert wide table to long table ") {
-    val query = createQuery(
-      """table WordField ,serialField, random_Field
-        || untable WordField, field, value""", "otstats", s"$test_index")
-    val actual = new Converter(query).run
+  test("Test 0. Command: | untable ") {
+    val actual = execute(""" table serialField, random_Field, WordField, junkField | untable WordField с1 с2""")
+//    val actual = execute(""" table serialField, random_Field, WordField | untable WordField c1 c2""")
+    println(actual)
     val expected = """[
-                     |{"WordField":"qwe","field":"serialField","value":"0"},
-                     |{"WordField":"qwe","field":"random_Field","value":"100"},
-                     |{"WordField":"rty","field":"serialField","value":"1"},
-                     |{"WordField":"rty","field":"random_Field","value":"-90"},
-                     |{"WordField":"uio","field":"serialField","value":"2"},
-                     |{"WordField":"uio","field":"random_Field","value":"50"},
-                     |{"WordField":"GreenPeace","field":"serialField","value":"3"},
-                     |{"WordField":"GreenPeace","field":"random_Field","value":"20"},
-                     |{"WordField":"fgh","field":"serialField","value":"4"},
-                     |{"WordField":"fgh","field":"random_Field","value":"30"},
-                     |{"WordField":"jkl","field":"serialField","value":"5"},
-                     |{"WordField":"jkl","field":"random_Field","value":"50"},
-                     |{"WordField":"zxc","field":"serialField","value":"6"},
-                     |{"WordField":"zxc","field":"random_Field","value":"60"},
-                     |{"WordField":"RUS","field":"serialField","value":"7"},
-                     |{"WordField":"RUS","field":"random_Field","value":"-100"},
-                     |{"WordField":"MMM","field":"serialField","value":"8"},
-                     |{"WordField":"MMM","field":"random_Field","value":"0"},
-                     |{"WordField":"USA","field":"serialField","value":"9"},
-                     |{"WordField":"USA","field":"random_Field","value":"10"}
+                     |{"WordField":"qwe","c1":"serialField","c2":"0"},
+                     |{"WordField":"qwe","c1":"random_Field","c2":"100"},
+                     |{"WordField":"rty","c1":"serialField","c2":"1"},
+                     |{"WordField":"rty","c1":"random_Field","c2":"-90"},
+                     |{"WordField":"uio","c1":"serialField","c2":"2"},
+                     |{"WordField":"uio","c1":"random_Field","c2":"50"},
+                     |{"WordField":"GreenPeace","c1":"serialField","c2":"3"},
+                     |{"WordField":"GreenPeace","c1":"random_Field","c2":"20"},
+                     |{"WordField":"fgh","c1":"serialField","c2":"4"},
+                     |{"WordField":"fgh","c1":"random_Field","c2":"30"},
+                     |{"WordField":"jkl","c1":"serialField","c2":"5"},
+                     |{"WordField":"jkl","c1":"random_Field","c2":"50"},
+                     |{"WordField":"zxc","c1":"serialField","c2":"6"},
+                     |{"WordField":"zxc","c1":"random_Field","c2":"60"},
+                     |{"WordField":"RUS","c1":"serialField","c2":"7"},
+                     |{"WordField":"RUS","c1":"random_Field","c2":"-100"},
+                     |{"WordField":"MMM","c1":"serialField","c2":"8"},
+                     |{"WordField":"MMM","c1":"random_Field","c2":"0"},
+                     |{"WordField":"USA","c1":"serialField","c2":"9"},
+                     |{"WordField":"USA","c1":"random_Field","c2":"10"}
                      |]""".stripMargin
-    compareDataFrames(actual, jsonToDf(expected))
-  }
-
-  test("Test 2. Command: Fixed column missing among data fields") {
-    val query = createQuery("""table serialField, random_Field
-        || untable WordField, field, value""", "otstats", s"$test_index")
-    var actual = new Converter(query).run
-    val expectedStr = """[
-                        |{"field":"serialField","value":"0"},
-                        |{"field":"random_Field","value":"100"},
-                        |{"field":"serialField","value":"1"},
-                        |{"field":"random_Field","value":"-90"},
-                        |{"field":"serialField","value":"2"},
-                        |{"field":"random_Field","value":"50"},
-                        |{"field":"serialField","value":"3"},
-                        |{"field":"random_Field","value":"20"},
-                        |{"field":"serialField","value":"4"},
-                        |{"field":"random_Field","value":"30"},
-                        |{"field":"serialField","value":"5"},
-                        |{"field":"random_Field","value":"50"},
-                        |{"field":"serialField","value":"6"},
-                        |{"field":"random_Field","value":"60"},
-                        |{"field":"serialField","value":"7"},
-                        |{"field":"random_Field","value":"-100"},
-                        |{"field":"serialField","value":"8"},
-                        |{"field":"random_Field","value":"0"},
-                        |{"field":"serialField","value":"9"},
-                        |{"field":"random_Field","value":"10"}
-                        |]""".stripMargin
-    var expected = jsonToDf(expectedStr).withColumn("WordField", F.lit(null))
-    actual = actual.select(actual.columns.sorted.toSeq.map(c => col(c)):_*)
-    expected = expected.select(expected.columns.sorted.toSeq.map(c => col(c)):_*)
-    compareDataFrames(actual, expected)
-  }
-
-  test("Test 3. Command: Fixed column contains nulls") {
-    val query = createQuery("""table serialField, random_Field
-    | table WordField ,serialField, random_Field
-    | untable WordField, field, value""", "otstats", s"$test_index")
-    var actual = new Converter(query).run
-    val expectedStr = """[
-                        |{"field":"serialField","value":"0"},
-                        |{"field":"random_Field","value":"100"},
-                        |{"field":"serialField","value":"1"},
-                        |{"field":"random_Field","value":"-90"},
-                        |{"field":"serialField","value":"2"},
-                        |{"field":"random_Field","value":"50"},
-                        |{"field":"serialField","value":"3"},
-                        |{"field":"random_Field","value":"20"},
-                        |{"field":"serialField","value":"4"},
-                        |{"field":"random_Field","value":"30"},
-                        |{"field":"serialField","value":"5"},
-                        |{"field":"random_Field","value":"50"},
-                        |{"field":"serialField","value":"6"},
-                        |{"field":"random_Field","value":"60"},
-                        |{"field":"serialField","value":"7"},
-                        |{"field":"random_Field","value":"-100"},
-                        |{"field":"serialField","value":"8"},
-                        |{"field":"random_Field","value":"0"},
-                        |{"field":"serialField","value":"9"},
-                        |{"field":"random_Field","value":"10"}
-                        |]""".stripMargin
-    var expected = jsonToDf(expectedStr).withColumn("WordField", F.lit(null))
-    actual = actual.select(actual.columns.sorted.toSeq.map(c => col(c)):_*)
-    expected = expected.select(expected.columns.sorted.toSeq.map(c => col(c)):_*)
-    compareDataFrames(actual, expected)
-  }
-
-  test("Test 3. Command: Call untable with 2 arguments ") {
-    val query = createQuery("""table WordField ,serialField, random_Field
-        || untable WordField, field""", "otstats", s"$test_index")
-    val actual = new Converter(query).run
-    compareDataFrames(actual, spark.emptyDataFrame)
+    assert(jsonCompare(actual, expected), f"Result : $actual\n---\nExpected : $expected")
   }
 
 }
