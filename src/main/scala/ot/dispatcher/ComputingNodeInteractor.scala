7f5b2e42a9d0cb311a71856ebdae514596f54ac5
DNikolaevKlg <dnikolaev@isgneuro.com>
2022-09-14 17:20:11 +0300

    Added computing node id generation from config, create facade class for SuperVisor's interaction with Kafka service.

---
+++ src/main/scala/ot/dispatcher/ComputingNodeInteractor.scala
@@ -0,0 +1,44 @@
+package ot.dispatcher
+
+import java.util.UUID
+
+class ComputingNodeInteractor(externalPort: Int) {
+  val superKafkaConnector = new SuperKafkaConnector(externalPort)
+
+  def registerNode(computingNodeUuid: UUID) = {
+    var hostId = UUID.randomUUID
+    while (hostId.compareTo(computingNodeUuid) == 0) {
+      hostId = UUID.randomUUID
+    }
+    val commandName = "REGISTER_COMPUTING_NODE"
+    val registerMessage =
+      s"""
+         |{
+         |"computing_node_uuid": "${computingNodeUuid}",
+         |"command_name": "${commandName}",
+         |"command": {
+         |    "computing_node_type": "SPARK",
+         |    "host_id": "${hostId}",
+         |    "otl_command_syntax": {},
+         |    "resources": {
+         |      "job_capacity": 999999999
+         |    }
+         |  }
+         |}
+         |""".stripMargin
+    superKafkaConnector.sendMessage("computing_node_control", commandName, registerMessage)
+  }
+
+  def unregisterNode(computingNodeUuid: UUID) = {
+    val commandName = "UNREGISTER_COMPUTING_NODE"
+    val unregisterMessage =
+      s"""
+         |{
+         |"computing_node_uuid": "${computingNodeUuid}",
+         |"command_name": "${commandName}",
+         |"command": {}
+         |}
+         |""".stripMargin
+    superKafkaConnector.sendMessage("computing_node_control", commandName, unregisterMessage)
+  }
+}
