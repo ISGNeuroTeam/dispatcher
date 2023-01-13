1dafe5524965e92052a17003a4afb7cd739ccd4a
DNikolaevKlg <dnikolaev@isgneuro.com>
2023-01-13 18:17:03 +0300

    Works by filldown optimization. Added defineTargetColumns parameter.

--- src/main/scala/ot/scalaotl/commands/OTLFilldown.scala
+++ src/main/scala/ot/scalaotl/commands/OTLFilldown.scala
@@ -6,19 +6,22 @@ import org.apache.spark.sql.expressions.Window
 import org.apache.spark.sql.functions._
 import ot.scalaotl.extensions.StringExt._
 
-import java.util.Calendar
-
 /** =Abstract=
  * This class provides support of __'''filldown'''__ otl command.
  *
  * __'''filldown'''__ changes __NULL__ values with the last non-null value for a field
  *
- * __'''filldown'''__ takes two optional argument:
+ * __'''filldown'''__ takes three optional argument:
  *
  *    1.  _'''wc-field-list'''_ - comma-separated field names to which the command will apply. If this param
  *    is empty, than command will apply to all NULL-value-containing fields
- *    2. __'''by'''__ - defines the field by which grouping occurs in the table to further replace
+ *
+ *    2. _'''by'''_ - defines the field by which grouping occurs in the table to further replace
  *    the null values of the fields specified in the _'''wc-field-list'''_ parameter separately for each group.
+ *
+ *    3. _'''defineTargetColumns'''_ - boolean parameter;
+ *    if true, than in case of missing _'''wc-field-list'''_ target (null values containing) columns defined automatically, else __'''filldown'''__ try to work on all columns.
+ *    In case, if _'''wc-field-list'''_ exists, this parameter will be ignored. Default value: false.
  *
  * Note: if _'''by'''_ is used, the parameter _'''wc-field-list'''_ should be specified.
  *
@@ -42,7 +45,26 @@ import java.util.Calendar
 |1650027083 | C|100|
 |1650027081 | C|5|
 +----------+---+---+}}}
- * OTL 2:
+ * * OTL 2:
+ * {{{| makeresults | eval a = mvappend(1,2,null,3), b ="A" | mvexpand a | append [makeresults | eval a = null,b="B"]
+ * | append [makeresults | eval a=4,b="A"] | append [makeresults | eval a=100,b="C"] | append [makeresults | eval a=null,b="A"]
+ * | append [makeresults | eval a = null,b="C"]  | append [makeresults | eval a = 5,b=null]  | sort b | filldown defineTargetColumns=true}}}
+ * Result:
+ * {{{+----------+---+---+
+|     _time|  b|  a|
++----------+---+---+
+|1650027083	| A|1|
+|1650027083	| A|2|
+|1650027083	| A|2|
+|1650027083	| A|2|
+|1650027083	| A|3|
+|1650027082 | A|4|
+|1650027082 | B|4|
+|1650027082 | C|4|
+|1650027083 | C|100|
+|1650027081 | C|5|
++----------+---+---+}}}
+ * OTL 3:
  * {{{| makeresults | eval a = mvappend(1,2,null,3), b ="A" | mvexpand a | append [makeresults | eval a = null,b="B"]
  * | append [makeresults | eval a=4,b="A"] | append [makeresults | eval a=100,b="C"] | append [makeresults | eval a=null,b="A"]
  * | append [makeresults | eval a = null,b="C"]  | sort b | filldown a}}}
@@ -60,7 +82,7 @@ import java.util.Calendar
 |1650027173 | C|4|
 |1650027174 | C|100|
 +----------+---+---+}}}
- * OTL 3:
+ * OTL 4:
  * {{{| makeresults | eval a = mvappend(1,2,null,3), b ="A" | mvexpand a | append [makeresults | eval a = null,b="B"]
  * | append [makeresults | eval a=4,b="A"] | append [makeresults | eval a=100,b="C"] | append [makeresults | eval a=null,b="A"]
  * | append [makeresults | eval a = null,b="C"]  | sort b | filldown a by b}}}
@@ -90,7 +112,6 @@ class OTLFilldown(sq: SimpleQuery) extends OTLBaseCommand(sq, _seps = Set("by"))
    * @return _df with events combined by specified field
    */
   override def transform(_df: DataFrame): DataFrame = {
-    val now1 = Calendar.getInstance().getTime
     //Define field for grouping. If by-param not exists, this field is fictive.
     val groups = positionalsMap.get("by") match {
       case Some(Positional("by", groups)) => groups
@@ -102,20 +123,13 @@ class OTLFilldown(sq: SimpleQuery) extends OTLBaseCommand(sq, _seps = Set("by"))
       groups.head.stripBackticks()
     }
     //Define fields for null replacing
-    val dfColumns = _df.columns.filter(cl => _df.schema.filter(s => s.nullable).map(_.name).contains(cl))
+    val dfColumns = _df.schema.filter(s => s.nullable).map(_.name).toList
     val fields = if (returns.flatFields.isEmpty) {
-      /*var query = ""
-      for ((c, i) <- dfColumns.zipWithIndex) {
-        query = query + (if (i > 0) {" or "} else {""})
-        query = query + s"$c is null"
+      keywordsMap.get("defineTargetColumns") match {
+        //if defineTargetColumns, than search columns, where null values exists, else all columns will be filldowned
+        case Some(Keyword("defineTargetColumns", "true")) => dfColumns.filter(c => !(_df.select(col(c)).filter(row => row.isNullAt(0)).isEmpty))
+        case _ => dfColumns
       }
-     // _df.schema.filter(s => s.)
-      val fdf = _df.filter(query)
-      val fdfView = fdf.c
-      val aaa = _df.select(col("ID"), when(col("ID").isNull, null))
-      val isa = aaa.isEmpty*/
-      val dfNulled = _df.filter(row => row.anyNull)
-      dfColumns.filter(c => !(_df.select(col(c)).filter(row => row.isNullAt(0)).isEmpty)).toList
     } else {
       returns.flatFields
     }
@@ -125,14 +139,12 @@ class OTLFilldown(sq: SimpleQuery) extends OTLBaseCommand(sq, _seps = Set("by"))
     //Window with ordering for grouping by by-param
     val ws = Window.partitionBy(by).orderBy("__idx__").rowsBetween(Window.unboundedPreceding, Window.currentRow)
     //Replace null values in filldown columns
-    val result = filldownedColumns.foldLeft(df_grouped.withColumn("__idx__", monotonically_increasing_id)) {
+    filldownedColumns.foldLeft(df_grouped.withColumn("__idx__", monotonically_increasing_id)) {
       case (accum, item) => {
         val column = last(col(item), ignoreNulls = true).over(ws)
         accum.withColumn(item, column)
       }
     }
       .drop("__idx__", "__internal__")
-    val now2 = Calendar.getInstance().getTime
-    result
   }
 }
--- src/test/scala/ot/scalaotl/commands/OTLFilldownTest.scala
+++ src/test/scala/ot/scalaotl/commands/OTLFilldownTest.scala
@@ -1,18 +1,19 @@
 package ot.scalaotl.commands
 
 class OTLFilldownTest extends CommandTest {
-  override val dataset = """[
-     {"_time": 0, "_raw": "{\"ID\":0,\"random_Field\": \"100\",\"field\": 10}"},
-     {"_time": 0, "_raw": "{\"ID\":0}"},
-     {"_time": 0, "_raw": "{\"ID\":0,\"random_Field\": \"150\"}"},
-     {"_time": 0, "_raw": "{\"ID\":0}"},
-     {"_time": 0, "_raw": "{\"ID\":1}"},
-     {"_time": 0, "_raw": "{\"ID\":1,\"random_Field\": \"830\",\"field\": 20}"},
-     {"_time": 0, "_raw": "{\"ID\":1}"},
-     {"_time": 0, "_raw": "{\"ID\":1}"}
-     ]"""
+  override val dataset =
+    """[
+       {"_time": 0, "_raw": "{\"ID\":0,\"random_Field\": \"100\",\"field\": 10}"},
+       {"_time": 0, "_raw": "{\"ID\":0}"},
+       {"_time": 0, "_raw": "{\"ID\":0,\"random_Field\": \"150\"}"},
+       {"_time": 0, "_raw": "{\"ID\":0}"},
+       {"_time": 0, "_raw": "{\"ID\":1}"},
+       {"_time": 0, "_raw": "{\"ID\":1,\"random_Field\": \"830\",\"field\": 20}"},
+       {"_time": 0, "_raw": "{\"ID\":1}"},
+       {"_time": 0, "_raw": "{\"ID\":1}"}
+       ]"""
 
-  test("Test 0. Command: table a | filldown") {
+  test("Test 0. Filldown without directly fields enumeration and without defining of them (filldown by all columns). Command: table a | filldown") {
     val actual = execute("""| filldown | table field, random_Field, ID""")
     val expected = """[
     {"field":"10","random_Field":"100","ID":"0"},
@@ -27,7 +28,23 @@ class OTLFilldownTest extends CommandTest {
     assert(jsonCompare(actual, expected), f"Result : $actual\n---\nExpected : $expected")
   }
 
-  test("Test 1. Command: table a | filldown field") {
+  test("Test 1. Filldown without directly fields enumeration and with defining of them (filldown by null containing columns). Command: table a | filldown defineTargetColumns=true") {
+    val actual = execute("""| filldown defineTargetColumns=true | table field, random_Field, ID""")
+    val expected =
+      """[
+    {"field":"10","random_Field":"100","ID":"0"},
+    {"field":"10","random_Field":"100","ID":"0"},
+    {"field":"10","random_Field":"150","ID":"0"},
+    {"field":"10","random_Field":"150","ID":"0"},
+    {"field":"10","random_Field":"150","ID":"1"},
+    {"field":"20","random_Field":"830","ID":"1"},
+    {"field":"20","random_Field":"830","ID":"1"},
+    {"field":"20","random_Field":"830","ID":"1"}
+    ]""".stripMargin
+    assert(jsonCompare(actual, expected), f"Result : $actual\n---\nExpected : $expected")
+  }
+
+  test("Test 2. Command: table a | filldown field") {
     val actual = execute("""| filldown field | table field """)
     val expected = """[
         {"field" : "10"},
@@ -42,23 +59,23 @@ class OTLFilldownTest extends CommandTest {
     assert(jsonCompare(actual, expected), f"Result : $actual\n---\nExpected : $expected")
   }
 
-  test("Test 2. Command: table a | filldown field, random_Field, ID") {
-    val actual = execute("""| filldown field, random_Field, ID | table field, random_Field, ID """)
+  test("Test 3. Command: table a | filldown field, random_Field") {
+    val actual = execute("""| filldown field, random_Field | table field, random_Field""")
     val expected =
       """[
-        {"field" : "10"},
-        {"field" : "10"},
-        {"field" : "10"},
-        {"field" : "10"},
-        {"field" : "10"},
-        {"field" : "20"},
-        {"field" : "20"},
-        {"field" : "20"}
+          {"field":"10","random_Field":"100"},
+          {"field":"10","random_Field":"100"},
+          {"field":"10","random_Field":"150"},
+          {"field":"10","random_Field":"150"},
+          {"field":"10","random_Field":"150"},
+          {"field":"20","random_Field":"830"},
+          {"field":"20","random_Field":"830"},
+          {"field":"20","random_Field":"830"}
         ]""".stripMargin
     assert(jsonCompare(actual, expected), f"Result : $actual\n---\nExpected : $expected")
   }
 
-  test("Test 3. Command: table a | filldown field by ID") {
+  test("Test 4. Command: table a | filldown field by ID") {
     val actual = execute("""| filldown field by ID | table field, ID """)
     val expected = """[
     {"field":"10","ID":"0"},
@@ -73,7 +90,7 @@ class OTLFilldownTest extends CommandTest {
     assert(jsonCompare(actual, expected), f"Result : $actual\n---\nExpected : $expected")
   }
 
-  test("Test 4. Command: table a | filldown field, random_Field by ID") {
+  test("Test 5. Command: table a | filldown field, random_Field by ID") {
     val actual = execute("""| filldown field, random_Field by ID | table field, random_Field, ID""")
     val expected = """[
     {"field":"10","random_Field":"100","ID":"0"},
