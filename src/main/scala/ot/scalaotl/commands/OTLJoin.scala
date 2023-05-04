decaeba17dced48310d9db5ff567a9110b4f7ce4
DNikolaevKlg <dnikolaev@isgneuro.com>
2023-05-04 12:51:09 +0300

    Join logic redeveloped for cases of columns with same names in joined dataframes.

--- src/main/scala/ot/scalaotl/commands/OTLJoin.scala
+++ src/main/scala/ot/scalaotl/commands/OTLJoin.scala
@@ -2,6 +2,8 @@ package ot.scalaotl
 package commands
 
 import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.functions._
+import ot.scalaotl.extensions.DataFrameExt._
 import ot.scalaotl.extensions.StringExt._
 
 class OTLJoin(sq: SimpleQuery) extends OTLBaseCommand(sq) {
@@ -16,6 +18,14 @@ class OTLJoin(sq: SimpleQuery) extends OTLBaseCommand(sq) {
       val jdf: DataFrame = cachedDFs(subsearch)
       val joinOn = returns.flatFields.map(_.stripBackticks())
       val bothCols = _df.columns.intersect(jdf.columns)
+      bothCols.foldLeft(_df){ case (accum, item) =>
+        val workItem = item.stripBackticks
+        accum.withSafeColumnRenamed(workItem, workItem + "__left__")
+      }
+      bothCols.foldLeft(jdf) { case (accum, item) =>
+        val workItem = item.stripBackticks
+        accum.withSafeColumnRenamed(workItem, workItem + "__right__")
+      }
       log.debug(f"[SearchId:${sq.searchId}] Columns in right df: ${jdf.columns.mkString(", ")}")
       log.debug(f"[SearchId:${sq.searchId}] Columns join on: ${joinOn.mkString(", ")}")
       log.debug(f"[SearchId:${sq.searchId}] Columns in both dashboards: ${bothCols.mkString(", ")}")
@@ -27,7 +37,13 @@ class OTLJoin(sq: SimpleQuery) extends OTLBaseCommand(sq) {
         }*/
         val suffView = dfSuffix.collect()
         val jdfView = jdf.collect()
-        val intdf = dfSuffix.join(jdf, joinOn, getKeyword("type").getOrElse("left"))
+        val intdf = dfSuffix.join(jdf, joinOn, getKeyword("type").getOrElse("left")).withColumn("__fake__", lit(null))
+        val intDfView = intdf.collect()
+        bothCols.foldLeft(intdf) { case (accum, item) =>
+          accum.withColumn(item, when(accum(item + "__right__") === accum("__fake__"), col(item + "__left__")).otherwise(col(item + "__right__")))
+          accum.drop(item + "__left").drop(item + "__right__")
+        }
+        intdf.drop("__fake__")
         if (getKeyword("max").getOrElse("0") == "1") {
           intdf.dropDuplicates(joinOn)
         } else {
