b8827bfbac4ef30e01f441e8b99370ff6830daec
DNikolaevKlg <dnikolaev@isgneuro.com>
2023-05-04 16:23:20 +0300

    Join logic redeveloped for cases of columns with same names in joined dataframes.

--- src/main/scala/ot/scalaotl/commands/OTLJoin.scala
+++ src/main/scala/ot/scalaotl/commands/OTLJoin.scala
@@ -3,7 +3,6 @@ package commands
 
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.functions._
-import ot.scalaotl.extensions.DataFrameExt._
 import ot.scalaotl.extensions.StringExt._
 
 class OTLJoin(sq: SimpleQuery) extends OTLBaseCommand(sq) {
@@ -12,19 +11,17 @@ class OTLJoin(sq: SimpleQuery) extends OTLBaseCommand(sq) {
   val cachedDFs: Map[String, DataFrame] = sq.cache
 
   override def transform(_df: DataFrame): DataFrame = {
-    val dfView = _df.collect()
     val subsearch: String = getKeyword("subsearch").getOrElse("__nosubsearch__")
     if (cachedDFs.contains(subsearch) && returns.flatFields.nonEmpty) {
       val jdf: DataFrame = cachedDFs(subsearch)
       val joinOn = returns.flatFields.map(_.stripBackticks())
       val bothCols = _df.columns.intersect(jdf.columns)
-      bothCols.foldLeft(_df){ case (accum, item) =>
-        val workItem = item.stripBackticks
-        accum.withSafeColumnRenamed(workItem, workItem + "__left__")
+      val notJoinBothCols = bothCols.diff(joinOn)
+      val workDf = notJoinBothCols.foldLeft(_df){ case (accum, item) =>
+        accum.withColumn(item + "__left__", col(item)).drop(item)
       }
-      bothCols.foldLeft(jdf) { case (accum, item) =>
-        val workItem = item.stripBackticks
-        accum.withSafeColumnRenamed(workItem, workItem + "__right__")
+      val workJdf = notJoinBothCols.foldLeft(jdf) { case (accum, item) =>
+        accum.withColumn(item + "__right__", col(item)).drop(item)
       }
       log.debug(f"[SearchId:${sq.searchId}] Columns in right df: ${jdf.columns.mkString(", ")}")
       log.debug(f"[SearchId:${sq.searchId}] Columns join on: ${joinOn.mkString(", ")}")
@@ -37,18 +34,18 @@ class OTLJoin(sq: SimpleQuery) extends OTLBaseCommand(sq) {
         }*/
         val suffView = dfSuffix.collect()
         val jdfView = jdf.collect()
-        val intdf = dfSuffix.join(jdf, joinOn, getKeyword("type").getOrElse("left")).withColumn("__fake__", lit(null))
+        val intdf = workDf.join(workJdf, joinOn, getKeyword("type").getOrElse("left")).withColumn("__fake__", lit(null))
         val intDfView = intdf.collect()
-        bothCols.foldLeft(intdf) { case (accum, item) =>
-          accum.withColumn(item, when(accum(item + "__right__") === accum("__fake__"), col(item + "__left__")).otherwise(col(item + "__right__")))
-          accum.drop(item + "__left").drop(item + "__right__")
+        var resultDf = notJoinBothCols.foldLeft(intdf) { case (accum, item) =>
+          accum.withColumn(item, when(accum(item + "__right__").isNull, col(item + "__left__")).otherwise(col(item + "__right__")))
+            .drop(item + "__left__").drop(item + "__right__")
         }
-        intdf.drop("__fake__")
+        resultDf = resultDf.drop("__fake__")
         if (getKeyword("max").getOrElse("0") == "1") {
-          intdf.dropDuplicates(joinOn)
+          resultDf.dropDuplicates(joinOn)
         } else {
-          val dfView = intdf.collect
-          intdf
+          val dfView = resultDf.collect
+          resultDf
         }
       } else _df
     } else _df
