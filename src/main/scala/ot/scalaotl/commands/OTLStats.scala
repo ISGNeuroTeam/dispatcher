a0c953687675c7771e52a110fe78407a73ff2ce5
DNikolaevKlg <dnikolaev@isgneuro.com>
2024-01-17 19:45:52 +0300

    Processing of stats command moved to otl_processors library.

--- src/main/scala/ot/scalaotl/commands/OTLStats.scala
+++ src/main/scala/ot/scalaotl/commands/OTLStats.scala
@@ -1,8 +1,9 @@
 package ot.scalaotl
 package commands
 
+import com.isgneuro.otl.processors.Stats
+import com.isgneuro.otl.processors.commonconstructions.StatsContext
 import org.apache.spark.sql.DataFrame
-import ot.scalaotl.commands.commonconstructions.{StatsContext, StatsTransformer}
 import ot.scalaotl.parsers.{StatsParser, WildcardParser}
 
 class OTLStats(sq: SimpleQuery) extends OTLBaseCommand(sq, _seps = Set("by")) with StatsParser with WildcardParser {
@@ -13,8 +14,18 @@ class OTLStats(sq: SimpleQuery) extends OTLBaseCommand(sq, _seps = Set("by")) wi
   override val fieldsGenerated: List[String] = getFieldsGenerated(returns)
 
   override def transform(_df: DataFrame): DataFrame = {
-    val statsTransformer = new StatsTransformer(Left(StatsContext(returns, positionalsMap, getKeyword("timeCol").getOrElse("_time"))), spark)
-    statsTransformer.transform(_df)
+    val statsFuncs = for (f <- returns.funcs)
+      yield com.isgneuro.scalaotl.core.StatsFunc(f.newfield, f.func, f.field)
+    val evals = returns.evals.map(e => (e.newfield -> e.expr)).toMap
+    val groupFields = positionalsMap get "by" match {
+      case Some(Positional("by", groups)) => groups
+      case _ => List()
+    }
+    val statsContext = StatsContext(statsFuncs, evals, groupFields, getKeyword("timeCol").getOrElse("_time"))
+    val stats = new Stats(spark, statsContext)
+    stats.transform(_df)
+    /*val statsTransformer = new StatsTransformer(Left(StatsContext(returns, positionalsMap, getKeyword("timeCol").getOrElse("_time"))), spark)
+    statsTransformer.transform(_df)*/
   }
 
 }
