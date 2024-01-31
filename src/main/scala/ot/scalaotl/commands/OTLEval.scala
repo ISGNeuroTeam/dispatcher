131ed336e3adcaab0fa6cd337a1ee47089d89fc5
DNikolaevKlg <dnikolaev@isgneuro.com>
2024-01-31 10:01:18 +0300

    Changed process of exception catching in eval: throw new exception, developing specially for eval with user-friendly error message.

--- src/main/scala/ot/scalaotl/commands/OTLEval.scala
+++ src/main/scala/ot/scalaotl/commands/OTLEval.scala
@@ -2,6 +2,7 @@ package ot.scalaotl
 package commands
 
 import com.isgneuro.otl.processors.Eval
+import com.isgneuro.scalaotl.exceptions.builders.EvalExceptionInvoker
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.functions.expr
 import ot.dispatcher.sdk.core.CustomException
@@ -11,12 +12,8 @@ import ot.scalaotl.parsers.ExpressionParser
 
 class OTLEval(sq: SimpleQuery) extends OTLBaseCommand(sq) with ExpressionParser {
 
-  import spark.implicits._
-
   val requiredKeywords = Set.empty[String]
   val optionalKeywords = Set.empty[String]
-
-  var errorInExpression: Option[ExpressionError] = None
 
   override def getFieldsUsed: Return => List[String] = (ret: Return) => {
     ret.evals.flatMap {
@@ -25,8 +22,7 @@ class OTLEval(sq: SimpleQuery) extends OTLBaseCommand(sq) with ExpressionParser 
         getFieldsFromExpression(expr(x.expr).expr, List()).map(_.stripBackticks().addSurroundedBackticks)
       } catch {
         case e: Exception =>
-          errorInExpression = Some(ExpressionError(e.getMessage, x.newfield))
-          List.empty[String]
+          EvalExceptionInvoker.doThrow(x.newfield, e.getMessage)
       }
     }
   }
@@ -37,20 +33,11 @@ class OTLEval(sq: SimpleQuery) extends OTLBaseCommand(sq) with ExpressionParser 
         throw E00020(sq.searchId, commandname)
     } catch {
       case e: CustomException =>
-        errorInExpression = Some(ExpressionError(e.getMessage, "No fields"))
+        EvalExceptionInvoker.doThrow("No fields", e.getMessage)
     }
   }
 
   override def transform(_df: DataFrame): DataFrame = {
-    errorInExpression match {
-      case Some(er) => Seq(
-        ("Error", er.message, er.field)
-      ).toDF("ResultType", "Message", "Column")
-      case None => calcEval(_df)
-    }
-  }
-
-  private def calcEval(_df: DataFrame): DataFrame = {
     val evalsMap = {
       for {e <- returns.evals}
         yield (e.newfield -> e.expr)
@@ -59,5 +46,3 @@ class OTLEval(sq: SimpleQuery) extends OTLBaseCommand(sq) with ExpressionParser 
     worker.transform(_df)
   }
 }
-
-case class ExpressionError(message: String, field: String)
